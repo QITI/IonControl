e2c936b573b9cd8fefdd816021049a0374184c86
Gilbert Shih <ldes89150@gmail.com>
2021-03-15 17:40:40 -0400

    modify the form of the exp fitting function

--- fit/FitFunctions.py
+++ fit/FitFunctions.py
@@ -206,7 +206,7 @@ class ChripedSinSqFit(FitFunctionBase):
 
 class ExponentialFit(FitFunctionBase):
     name = "Exponential"
-    functionString = 'A*(exp(-x/T)-B)'
+    functionString = 'A*exp(-x/T)+B'
     parameterNames = [  'T', 'A', 'B' ]
     def __init__(self):
         FitFunctionBase.__init__(self)
@@ -214,7 +214,7 @@ class ExponentialFit(FitFunctionBase):
         self.startParameters = [100, 1, 0]
         
     def functionEval(self, x, T, A, B ):
-        return A * (numpy.exp(-x / T) - B)
+        return A * numpy.exp(-x / T) + B
     
 class SaturationFit(FitFunctionBase):
     name = "Saturation"
